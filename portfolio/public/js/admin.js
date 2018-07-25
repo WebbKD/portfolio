66d8ab7bbeb63fb79f7fb9f3a11b7cbc72cc31ac
webbkd <kdw1995@live.com>
2018-07-24 21:18:47 -0400

    Adding comments to the admin js

--- portfolio/public/js/admin.js
+++ portfolio/public/js/admin.js
@@ -1,19 +1,19 @@
-var createPostBtn = document.querySelector('#createPostBtn'),
-    xhr = new XMLHttpRequest;
-console.log(createPostBtn);
+var createPostBtn = document.querySelector('#createPostBtn');
 
 createPostBtn.addEventListener("click",function(){
+    //getting the laravel token 
     let token = document.querySelector('[name=csrf-token]').getAttribute('content');
     let url = '/storePost';
     console.log(token);
   
-    
+    //grabbing the data from the form  
     let title = document.querySelector('#title').value; 
     let category = document.querySelector('#category').value;
     let link = document.querySelector("#sitelink").value;
     let img = document.querySelector('#postImg').value;
     
     console.log(title, category, link, img);
+    //use the fetch function to send the data from the form to the database
     fetch(url,{
         headers:{
             "Content-Type": "application/json",
